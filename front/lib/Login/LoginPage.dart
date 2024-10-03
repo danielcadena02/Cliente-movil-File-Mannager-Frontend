3326530b1f1add192aa4f5b911c20e4d9e7af526
angieclemus <angielem11@gmail.com>
2024-10-02 23:09:51 -0500

    Update LoginPage.dart

--- front/lib/Login/LoginPage.dart
+++ front/lib/Login/LoginPage.dart
@@ -59,71 +59,69 @@ class LoginPage extends StatelessWidget {
         backgroundColor: const Color(0xFF536492),
       ),
       body: Container(
-          color: Colors.white,
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Inicia sesión',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.grey[800],
+        color: Colors.white,
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Inicia sesión',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.grey[800],
+              ),
+            ),
+            const SizedBox(height: 20),
+            TextFormField(
+              onChanged: (value) => username = value,
+              decoration: const InputDecoration(
+                labelText: 'Correo electrónico',
+                filled: true,
+                fillColor: Colors.white,
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Color(0xFF536492)),
                 ),
               ),
-              const SizedBox(height: 20),
-              TextFormField(
-                onChanged: (value) => username = value,
-                decoration: const InputDecoration(
-                  labelText: 'Correo electrónico',
-                  filled: true,
-                  fillColor: Colors.white,
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Color(0xFF536492)),
-                  ),
+            ),
+            const SizedBox(height: 20),
+            TextFormField(
+              onChanged: (value) => password = value,
+              obscureText: true,
+              decoration: const InputDecoration(
+                labelText: 'Contraseña',
+                filled: true,
+                fillColor: Colors.white,
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Color(0xFF536492)),
                 ),
               ),
-              const SizedBox(height: 20),
-              TextFormField(
-                onChanged: (value) => password = value,
-                obscureText: true,
-                decoration: const InputDecoration(
-                  labelText: 'Contraseña',
-                  filled: true,
-                  fillColor: Colors.white,
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Color(0xFF536492)),
-                  ),
-                ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () => login(context),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Color(0xFF536492)),
               ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  // Navegar a la pantalla de inicio (HomePage) sin hacer autenticación real
-                  Get.to(HomePage());
-                },
-                style: ButtonStyle(
-                  backgroundColor: WidgetStatePropertyAll(Color(0xFF536492))
-                ),
-                child: const Text(
-                  'Iniciar sesión',
-                  style: TextStyle(color: Colors.white),
-                ),
+              child: const Text(
+                'Iniciar sesión',
+                style: TextStyle(color: Colors.white),
               ),
-              const SizedBox(height: 10),
-              TextButton(
-                onPressed: () {
-                  // Navegar a la pantalla de registro (RegisterPage)
-                  Get.to(RegisterPage());
-                },
-                child: Text(
-                  '¿No tienes cuenta? Registrarse',
-                  style: TextStyle(color: Color(0xFF536492)),
-                ),
+            ),
+            const SizedBox(height: 10),
+            TextButton(
+              onPressed: () {
+                // Navegar a la pantalla de registro (RegisterPage)
+                Get.to(RegisterPage());
+              },
+              child: Text(
+                '¿No tienes cuenta? Registrarse',
+                style: TextStyle(color: Color(0xFF536492)),
               ),
-            ],
-          )),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
